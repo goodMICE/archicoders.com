74eba654da4d8467d6b194bb0aaed13cc69aedad
goodMICE <hamel2517@gmail.com>
2017-04-01 08:15:38 +0600

    12'th commit

--- cats/chat/index.php
+++ cats/chat/index.php
@@ -1,4 +1,9 @@
 <?php
-include '/scripts/chat.php';
+include './../login/scripts/login.php';
+if(isset($_COOKIE['arhicspass']) && isset($_COOKIE['arhicslogin']))
+	$is_logined = check_data(htmlspecialchars($_COOKIE['arhicslogin']), htmlspecialchars($_COOKIE['arhicspass']));
+if(!$is_logined)
+		header ('Location: http://arhicoders.ru/arhicoders.com/cats/login/');
 include 'index.html';
+include './scripts/chat.php';
 ?>
